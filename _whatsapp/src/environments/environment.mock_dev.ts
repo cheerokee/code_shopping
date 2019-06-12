2e1c3ebc955b76d986034d5052b1204d71699e52
Jonas Inhani Martins <jonasmweb@hotmail.com>
2019-06-11 23:51:21 -0300

    Alterações

---
+++ _whatsapp/src/environments/environment.mock_dev.ts
@@ -0,0 +1,15 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  api: {
+    protocolo: 'http',
+    host: '192.168.0.100:8000',
+    get url(){
+      return `${this.protocolo}://${this.host}/api`
+    }
+  },
+  baseFilesUrl: 'http://192.168.0.100:8000/storage'
+};
